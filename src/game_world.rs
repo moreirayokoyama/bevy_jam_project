73702e1741779eb4af6a73633ad04915e4095578
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-07-26 16:54:19 -0300

    Fixing single block height

--- src/game_world.rs
+++ src/game_world.rs
@@ -18,7 +18,23 @@ impl GameWorld {
     }
 
     pub fn get_height_in_blocks(&self, x: usize) -> f32 {
-        self.surface_height[x % WORLD_WIDTH].trunc()
+        let height = self.surface_height[x % WORLD_WIDTH].trunc();
+        let left_height = if x > 0 {
+            self.surface_height[x - 1].trunc()
+        } else {
+            self.surface_height[WORLD_WIDTH - 1].trunc()
+        };
+        let right_height = if x < WORLD_WIDTH {
+            self.surface_height[x + 1].trunc()
+        } else {
+            self.surface_height[0].trunc()
+        };
+
+        if height > left_height && height > right_height {
+            left_height.max(right_height)
+        } else {
+            height
+        }
     }
 
     pub fn get_block_position(x: usize, y: usize) -> Vec2 {
